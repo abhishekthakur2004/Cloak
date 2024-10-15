2b1768945b6ce6da69c389361ea42bf4092a9b58
abhishek-217 <abhiman5196@gmail.com>
2024-10-15 19:42:33 +0530

    First Commit of popup page

---
+++ Data/Popup/popup.js
@@ -0,0 +1,5 @@
+// document.getElementById('blurBtn').addEventListener('click', () => {
+//     chrome.tabs.executeScript({
+//       code: 'blurSensitiveInfo();'
+//     });
+//   });
---
+++ content.js
@@ -0,0 +1,7 @@
+// function blurSensitiveInfo() {
+//     const emailPattern = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i;
+//     document.body.innerHTML = document.body.innerHTML.replace(emailPattern, match => {
+//       return `<span style="background-color: gray; filter: blur(5px);">${match}</span>`;
+//     });
+//   }
+  
